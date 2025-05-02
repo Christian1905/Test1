ff7feff51c7029c169e8ea735083dddf402ab293
Christian1905 <158076892+Christian1905@users.noreply.github.com>
2025-05-02 06:07:11 -0500

    Add files via upload

---
+++ src/app/models/ipLocation.model.ts
@@ -0,0 +1,38 @@
+export interface IpLocationResponse {
+  ip: string;
+  type: string;
+  continent_code: string;
+  continent_name: string;
+  country_code: string;
+  country_name: string;
+  region_code: string;
+  region_name: string;
+  city: string;
+  zip: string;
+  latitude: number;
+  longitude: number;
+  location: Location;
+  success?: boolean;
+  error?: {
+    code: number;
+    type: string;
+    info: string;
+  };
+}
+
+export interface Location {
+  geoname_id: number;
+  capital: string;
+  languages: Language[];
+  country_flag: string;
+  country_flag_emoji: string;
+  country_flag_emoji_unicode: string;
+  calling_code: string;
+  is_eu: boolean;
+}
+
+export interface Language {
+  code: string;
+  name: string;
+  native: string;
+}
